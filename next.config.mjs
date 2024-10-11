47a281912dd413ce444b45c58286bbd37be0d707
Manoj Ahirwar <manojahirwar11@gmail.com>
2024-10-11 11:45:48 +0530

    blog

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://manoj.usearticle.com", // Proxy the /blog route
+      },
+      {
+        source: "/blog/:slug*",
+        destination: "https://manoj.usearticle.com/:slug*", // Proxy the /blog/slug routes
+      },
+    ];
+  },
 };
+
+console.log("Next config loaded");
 
 export default nextConfig;
